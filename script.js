dd23dc5311d76a1f8e700996e3e425bd3042dc4c
Y-A2 <y.ahmed9@share.epsb.ca>
2025-03-21 13:55:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("fir").addEventListener("click", sentence1);
+document.getElementById("sec").addEventListener("click", sentence2);
+document.getElementById("thr").addEventListener("click", sentence3);
+document.getElementById("fou").addEventListener("click", sentence4);
+document.getElementById("fif").addEventListener("click", sentence5);
+
+
+function sentence1()
+{
+alert("leg")
+}
+
+function sentence2()
+{
+alert("day")
+}
+
+function sentence3()
+{
+alert("slack")
+}
+
+function sentence4()
+{
+alert("devil")
+}
+
+function sentence5()
+{
+alert("gain")
+}
+
+
